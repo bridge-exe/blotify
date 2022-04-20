25ae5a557a4a517939d21082c644089fb665b1b1
bridge.exe <62908125+bridge-exe@users.noreply.github.com>
2022-04-19 20:15:13 -0700

    added inputs for userid, clienid, secret, and playlist

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
-from auth import * 
+from auth import *
+from dom_color import *
+from sort import sorter
+import os
 
-client_id = 'ca352b7949dd41018f9fd85dc2aeb024'
-secret = '17314964a6e14136b05c9bdbd9234ae1'
+#
+print('Please input your client_id, secret, and your personal user_id.\n') 
+client_id = str(input("Your client ID: "))
+secret = str(input("Your client secret: "))
+user_is = str(input("Your user ID"))
+playlist_to_sort = str(input("Exact name of playlist to sort:"))
+# playlist_to_sort = ''
+
 
 auth = SpotifyClientCredentials(client_id, secret)
 sp = spotipy.Spotify(auth_manager=auth)
 
-user_id = 's3haqvhbykrrknwz1c0wwukwn'
-playlist_to_sort = 'Copy//Paste'
+
 
 playlists = sp.user_playlists('spotify')
-user_playlists = sp.user_playlists(user_id)
+user_playlists = sp.user_playlists(user_id) 
 
 def playlister(user, playlists, to_sort):
     pl_uri_list = {}
@@ -37,6 +45,18 @@ def playlister(user, playlists, to_sort):
         song_art  = playlist_items['items'][song]['track']['album']['images'][0]['url']
         song_and_art[song_uri] = song_art
         
+    print('playlister done')
     return song_and_art
-        
-        
+    
+# spotify_client = SpotifyClient(os.getenv("SPOTIFY_AUTHORIZATION_TOKEN"),
+#                                    os.getenv("SPOTIFY_USER_ID"))
+p = playlister(user_id, user_playlists, playlist_to_sort)
+d = dom_col(p)
+tracklist = sorter(d)
+
+if len(tracklist) > 0: 
+    sorted_playlist = playlist_to_sort + ' Sorted HSV'
+    new_list = sp.user_playlist_create(user_id, sorted_playlist)
+
+    playlist_id = new_list["id"]
+    sp.playlist_add_items(playlist_id, tracklist)
